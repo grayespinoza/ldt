2dc1aff1cb00b5d083b15fcf9e65ba71974ddf9f
Grey <grey@grayespinoza.dev>
2025-02-18 12:51:22 -0800

    removed logging

--- src/main/java/dev/heygrey/LookDownTransparency.java
+++ src/main/java/dev/heygrey/LookDownTransparency.java
@@ -2,15 +2,8 @@ package dev.heygrey;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class LookDownTransparency implements ModInitializer {
-  public static final String MOD_ID = "look-down-transparency";
-  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
   @Override
   public void onInitialize() {
-    LOGGER.info("Hello Fabric world!");
   }
 }
