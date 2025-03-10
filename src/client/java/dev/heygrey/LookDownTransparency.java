091519be78c370e8aa186a1082261e62f5ec430e
Grey <grey@grayespinoza.dev>
2025-03-09 22:35:49 -0700

    restructured for client implementation

---
+++ src/client/java/dev/heygrey/LookDownTransparency.java
@@ -0,0 +1,9 @@
+package dev.heygrey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LookDownTransparency implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+  }
+}
--- src/main/java/dev/heygrey/LookDownTransparency.java
+++
@@ -1,9 +0,0 @@
-package dev.heygrey;
-
-import net.fabricmc.api.ModInitializer;
-
-public class LookDownTransparency implements ModInitializer {
-  @Override
-  public void onInitialize() {
-  }
-}
